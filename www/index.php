89515a6f8276cd5774caf572f1882b61d589cf02
slepic <gslepic@gmail.com>
2019-01-09 17:16:17 +0000

    jebat nette

--- www/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-/** @var Nette\DI\Container $container */
-$container = require __DIR__ . '/../app/bootstrap.php';
-
-$container->getByType(Nette\Application\Application::class)
-	->run();
